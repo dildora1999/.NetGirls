eb140bb3f59eea74c187a02b16b918ea1bfeb451
Dildora Ganieva <dganieva@appulate.com>
2022-11-12 14:12:53 +0500

    First commit, 1,7 task

---
+++ FirstWeekTasks/FirstWeekTasks/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FirstWeekTasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Hello, input the task number, please: ");
+        string? taskNumber = Console.ReadLine();
+        switch (taskNumber)
+        {
+            case "1":
+                HelloWorld();
+                break;
+            case "7":
+                MultiplicationTable();
+                break;
+        }
+    }
+
+    private static void HelloWorld()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static void MultiplicationTable()
+    {
+        Console.Write("Please input the number for table: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= 12; i++)
+            Console.Write("{0} * {1} = {2} \n", number, i, number * i);
+    }
+}
